c0615ddf3d8ae6abfcc3907a741574c2dac52618
jackpickard <jack.pickard@hotmail.com>
2020-07-09 17:23:59 +0100

    adding small change to browser that helps running

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 from src.bloodstock import (weatherby,
                             non_weatherby,
@@ -20,10 +21,13 @@ df2 = create_df2_template()
 if __name__ == '__main__':
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     browser.get('http://www.racingpost.com')
-    print('Initialising headless chrome')
+    search_form=browser.find_elements_by_xpath('//a[@class="CybotCookiebotDialogBodyButton"]')
+    time.sleep(1)
+    search_form[1].click()
+    print('Initialising chrome')
     df1, df2 = run_bloodstock(browser, horse_ls, df1, df2)
-    df1.to_csv('~/Downloads/stallion_basic_stats.csv')
-    df2.to_csv('~/Downloads/stallion_progeny_stats.csv')
+    df1.to_csv('~/Downloads/stallion_basic_stats.csv') ## TODO add to database
+    df2.to_csv('~/Downloads/stallion_progeny_stats.csv') ## TODO add to database
     print('Finished scraping')
 
 
